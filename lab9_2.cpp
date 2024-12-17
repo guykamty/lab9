22c184f0b9b74af17bcbcfb83cb74960d967a509
guykamty <guykamty@gmail.com>
2024-12-17 14:49:26 +0700

    lab9_2_complete

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 
 //Write the function printO() here
+//ขอให้เติมมาเฉพาะ function printO() โดยที่ไม่ต้องเติม function main() มาในช่องนี้
+void printO(int N, int M) {
+    if (N <= 0 || M <= 0) {
+        cout << "Invalid input" << endl;
+    }
+   
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            cout << "O";
+        }
+        cout << endl;
+    }
+}
 
 int main(){
 	
